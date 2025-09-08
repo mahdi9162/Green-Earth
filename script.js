96d6f9e5908d0eaf425e6376892c59fc12fd7273
Mahdi Hasan <hasanmahdi6000@gmail.com>
2025-09-08 22:56:50 +0600

    Add All Plants in Home Page

--- script.js
+++ script.js
@@ -10,6 +10,14 @@ const categorieNameData = async () => {
   displayNameData(nameData.categories);
 };
 
+const allPlantsData = async () => {
+  const url = 'https://openapi.programming-hero.com/api/plants';
+  const res = await fetch(url);
+  const plantsDataAll = await res.json();
+  displayAllPlants(plantsDataAll.plants);
+};
+allPlantsData();
+
 
 
 // All Display Data is Here =>
@@ -24,4 +32,27 @@ const displayNameData = (categories) => {
   });
 };
 categorieNameData();
+
+const displayAllPlants = (allPlantsData) => {
+  allPlantsData.forEach((plant) => {
+    const newDiv = document.createElement('div');
+    newDiv.innerHTML = `
+    <div id="div-card" class="bg-white hover:translate-y-1 hover:scale-[1.02] transition-all duration-500 h-full flex flex-col rounded-xl p-4 shadow-sm">
+              <div id="img-div" class="h-[178px] overflow-hidden rounded-lg mb-2"><img class="w-full h-full object-cover" src="${plant.image}" alt="" /></div>
+              <div class="flex flex-col grow">
+                <h2 onclick="plantDetailsId(${plant.id})" class="font-semibold cursor-pointer">${plant.name}</h2>
+                <p class="leading-relaxed nunito-font text-sm my-3 line-clamp-3">
+                  ${plant.description}</p>
+                <div class="flex justify-between items-center">
+                  <button id="category-btn" class="text-[#15803d] text-sm bg-[#dcfce7] px-3 py-1 rounded-full mb-3">${plant.category}</button>
+                  <p class="font-semibold text-sm">
+                    ৳<span>${plant.price}</span>
+                  </p>
+                </div>
+                <button id="cart-btn" class="btn w-full text-white rounded-full mt-auto bg-[#15803d]">Add to Cart</button>
+              </div>
+    </div>`;
+    cardContainer.appendChild(newDiv);
+  });
+};
 
