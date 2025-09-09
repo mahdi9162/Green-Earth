c51ff6ecf69993fe5abc3a9c70a26fe66f4a1035
Mahdi Hasan <hasanmahdi6000@gmail.com>
2025-09-09 12:22:02 +0600

    Add Trees By Category

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const totalPriceContainer = document.getElementById('total-price-container');
 const priceCardsContainer = document.getElementById('price-cards-container');
 const cartContainer = document.getElementById('cart-container');
 
+
 // All Fetch is Here =>
 // Load Categories Names Data
 const categorieNameData = async () => {
@@ -35,8 +36,7 @@ const displayNameData = (categories) => {
   categories.forEach((cat) => {
     const newDiv = document.createElement('div');
     newDiv.innerHTML = `
-    <div class="text-black cursor-pointer hover:bg-[#15803d] transition-all duration-500 bg-gray-100 hover:text-white p-2 rounded-sm"><button class="cursor-pointer">${cat.category_name}</button></div>
-    `;
+    <div id="category-button" onclick= "categoryData(${cat.id})" class="text-black cursor-pointer hover:bg-[#15803d] transition-all duration-500 bg-gray-100 hover:text-white p-2 rounded-sm"><button class="cursor-pointer">${cat.category_name}</button></div>`;
     categorieContainer.appendChild(newDiv);
   });
 };
@@ -47,7 +47,7 @@ const displayAllPlants = (allPlantsData) => {
   allPlantsData.forEach((plant) => {
     const newDiv = document.createElement('div');
     newDiv.innerHTML = `
-    <div id="div-card" class="bg-white hover:translate-y-1 hover:scale-[1.02] transition-all duration-500 h-full flex flex-col rounded-xl p-4 shadow-sm">
+    <div id="div-card" class="bg-white hover:translate-y-1 hover:scale-[1.02] transition-all duration-500 h-[400px] flex flex-col rounded-xl p-4 shadow-sm">
               <div id="img-div" class="h-[178px] overflow-hidden rounded-lg mb-2"><img class="w-full h-full object-cover" src="${plant.image}" alt="" /></div>
               <div class="flex flex-col grow">
                 <h2 onclick="plantDetailsId(${plant.id})"  class="font-semibold cursor-pointer">${plant.name}</h2>
@@ -102,8 +102,8 @@ cardContainer.addEventListener('click', (e) => {
     const name = cartBtn.parentNode.children[0].textContent;
     const price = Number(cartBtn.parentNode.children[2].children[1].children[0].textContent);
     totalPriceContainer.classList.remove('hidden');
-      const newDiv = document.createElement('div');
-      newDiv.innerHTML = `
+    const newDiv = document.createElement('div');
+    newDiv.innerHTML = `
                 <div id="price-card" class="flex items-center justify-between rounded-lg bg-[#F0FDF4] py-2 px-3 my-2">
               <div id="price-card-text" class="">
                 <p class="leading-relaxed font-semibold">${name}</p>
@@ -145,3 +145,13 @@ priceCardsContainer.addEventListener('click', (e) => {
     deleteCart.remove();
   }
 });
+
+// Category Load Data
+const categoryData = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/category/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  cardContainer.innerHTML = '';
+  displayAllPlants(data.plants);
+};
+
